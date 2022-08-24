f43021ac4ab0892092805300ceb19f4387d69ef3
Connor Mullan <cmullan602@gmail.com>
2022-08-23 19:46:59 -0700

    changed schema.sql.

--- db/schema.sql
+++ db/schema.sql
@@ -3,26 +3,28 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
 
-CREATE TABLE departments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    names VARCHAR(30) NOT NULL
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
+    salary DECIMAL (6,0) NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
-CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-     CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
-    manager_id INT
-    --  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+    role_id INTEGER,
+    manager_id INTEGER REFERENCES employee(id) ON DELETE SET NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO departments (names)
+INSERT INTO department (name)
 VALUES ("Sales"),
        ("Marketing"),
        ("Engineering"),
        ("IT"),
        ("Administration");
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 values ("District Manager", 90000, 1),
        ("Digital Marketing", 80000, 2),
        ("Project Manager", 100000, 3),
@@ -13,7 +13,7 @@ values ("District Manager", 90000, 1),
        ("Customer Service", 60000, 5);
 
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Scooby", "Doo", 1, null),
        ("Daphney", "Duck", 1, 1),
        ("Lance", "Armstrong", 2, null),
